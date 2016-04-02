0f0965d55bb471350360378ba2a115531310708d
Marina <mrtihova@gmail.com>
2016-04-02 14:50:54 +0300

    Promenih main.c da e prosto return 0

--- Gosho/main.c
+++ Gosho/main.c
@@ -1,23 +1,7 @@
-#ifndef MOTOR_CONTROL_H
-#define MOTOR_CONTROL_H
-
 #include <kipr/botball.h>
+#include "motors_control"
 
-#define LIGHT_SENSOR_PORT 1
-#define START_SENSOR_PORT 0
-#define PIPE_PORT 3
-#define BOT_PORT 2
-#define RIGHT_MOTOR_PORT 1
-#define LEFT_MOTOR_PORT 0 
-#define SOLAR_ARRAY_PORT 1
-#define LIFT_BOT_PORT 0
-
-void drive_straight(int);
-void drive_backwards(int);
-void turn_left(int);
-void turn_right(int);
-void drive_left(int);
-void drive_right(int);
-
-
-#endif
+int main()
+{
+  return 0;
+}
